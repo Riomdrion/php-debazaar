77c900d5f138953e9743d0694acb2723e7340e90
Rodin van den Berg <rma.vandenberg@student.avans.nl>
2025-04-05 15:48:22 +0200

    test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,27 +2,18 @@
 
 namespace Tests\Browser;
 
-use App\Models\User;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class LoginTest extends DuskTestCase
 {
-    use DatabaseMigrations;
-
     public function test_user_can_login()
     {
-        $user = User::factory()->create([
-            'email' => 'rodin@example.com',
-            'password' => bcrypt('password4'),
-        ]);
-
-        $this->browse(function (Browser $browser) use ($user) {
+        $this->browse(function (Browser $browser) {
             $browser->visit('/login')
-                ->type('email', $user->email)
-                ->type('password', 'secret')
-                ->press(' log in ')
+                ->type('email', 'rodin@example.com')
+                ->type('password', 'password4')
+                ->press('log in')
                 ->assertPathIs('/dashboard');
         });
     }
