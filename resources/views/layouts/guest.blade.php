f6dfa005e52c4ed23abff97cb353351f544cd37a
Rodin van den Berg <rma.vandenberg@student.avans.nl>
2025-04-05 12:17:07 +0200

    favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'The Bazaar') }}</title>
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
