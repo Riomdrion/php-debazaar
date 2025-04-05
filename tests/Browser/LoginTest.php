d700d8e582cf10d7bed2dc65a0673312c51034a7
Rodin van den Berg <rma.vandenberg@student.avans.nl>
2025-04-05 14:41:19 +0200

    dusk startup

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    public function testSanity(): void
+    {
+        $this->assertTrue(true);
+    }
+}
